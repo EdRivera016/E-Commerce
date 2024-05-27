e01d5e1131f980f0911d567fc29a57e1b835501c
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-05-27 01:26:51 -0400

    did products routes

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
       dialectOptions: {
         decimalNumbers: true,
       },
