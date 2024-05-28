bfe89f199b2afda3f5c57d332e07401922b5f0f9
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-05-27 22:34:24 -0400

    testing api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,7 +26,8 @@ try {
 
   res.status(200).json(category);
 } catch (err) {
-  res.status(500).json(err);
+  console.error(err);
+  res.status(500).json({message: 'An error occured while fetching the category!', error: err.message });
 }
 });
 
